f6287bf30a34d013fe0a6b062c0a5400a8cbe13e
Justin Coyne <jcoyne@justincoyne.com>
2017-08-27 09:11:15 -0500

    Remove autofocus polyfill
    autofocus is supported by all browsers (except mobile Safari where
    focusing without interaction is not desired as it pops up a keyboard)

--- app/assets/javascripts/blacklight/blacklight.js
+++ app/assets/javascripts/blacklight/blacklight.js
@@ -5,7 +5,6 @@
 //
 // These javascript files are compiled in via the Rails asset pipeline:
 //= require blacklight/core
-//= require blacklight/autofocus
 //= require blacklight/bookmark_toggle
 //= require blacklight/modal
 //= require blacklight/search_context
@@ -25,32 +24,32 @@
 // Twitter Typeahead for autocomplete
 //= require twitter/typeahead
 
-/* Blacklight has a Javascript setup meant to support local disabling, 
-  modification, and use of Blacklight behaviors. 
-  
-  There is a global Blacklight object, available to your local JS. 
-  
-  Individual logic to apply JS behaviors to particular elements is 
-  stored in functions on that Blacklight object. 
-  
-  The actual behaviors themselves are implemented as JQuery plugins, 
+/* Blacklight has a Javascript setup meant to support local disabling,
+  modification, and use of Blacklight behaviors.
+
+  There is a global Blacklight object, available to your local JS.
+
+  Individual logic to apply JS behaviors to particular elements is
+  stored in functions on that Blacklight object.
+
+  The actual behaviors themselves are implemented as JQuery plugins,
   JQuery-UI widgets (a special kind of JQuery plugin), or in some cases
-  just as logic in the Blacklight global object. 
-  
+  just as logic in the Blacklight global object.
+
   All of these things can be modified by your local JS code -- these functions
   are all set up on js load, and only called on document ready, so do your
   modifications just on js load, and they'll be made by the time document ready
-  comes along. 
-  
+  comes along.
+
   Examples, in your application's own JS:
-        
+
     Turn off adding of behavior to facet 'more' links, using a no-op function:
-    
+
         Blacklight.do_more_facets_behavior = function() {};
-        
+
     Change the implementation of facet 'more' link behavior to use entirely
-    different JS. 
-    
+    different JS.
+
         Blacklight.do_more_facets_behavior = function() {
           $(Blacklight.do_more_facets_behavior.selector).each(function() {
             //my own thing!
